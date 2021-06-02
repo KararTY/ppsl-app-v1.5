380e4af67742fdd15afd98cfebb0cf39bc9a4f68
KararTY <KararTY@users.noreply.github.com>
2021-06-02 18:21:08 +0000

    deploy: 08d2872ed7c8ea15410f80899205bf77acd867fd

---
+++ dist/Mockups/Random.js
@@ -0,0 +1,31 @@
+import { DateTime } from '../../_snowpack/pkg/luxon.js'
+import Server from '../Icons/Server.svelte.js'
+import Monitor from '../Icons/Monitor.svelte.js'
+
+const n = () => (Math.random() * 10).toFixed(0)
+
+export function randomNumber () {
+  return `${n()}${n()}${n()}-${n()}${n()}${n()}${n()}`
+}
+
+export function randomYear () {
+  return `20${n()}${n()}`
+}
+
+export function randomProduct () {
+  const products = ['AnanasBook', 'CherryBook', 'PearBook', 'OrangeBook']
+  return products[Math.floor(Math.random() * products.length)]
+}
+
+export function randomTitle (year, product) {
+  return `A${n()}${n()}${n()}${n()} ${year || randomYear()} ${n()}${n()}” ${product || randomProduct()}`
+}
+
+export function randomDateTime () {
+  return DateTime.fromJSDate(new Date(+(new Date()) - Math.floor(Math.random() * 10000000000)))
+}
+
+export function randomIcon () {
+  const icons = [Server, Monitor]
+  return icons[Math.floor(Math.random() * icons.length)]
+}
